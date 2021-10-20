3c10b9b304506fcf595c3a986c090a9e8039a513
leandro.antonelli@gmail.com <leandro.antonelli@gmail.com>
2021-10-20 13:09:04 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifExpertApp from './GifExpertApp';
+//import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const texto = 'Hola Leandro';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const container = (
+// 	<Container fluid className="mt-3 p-4">
+// 		<Row>
+// 			<Col>
+
+// 				<Form>
+// 					<Form.Group className="mb-3" controlId="formBasicEmail">
+// 						<Form.Label>Email address</Form.Label>
+// 						<Form.Control type="email" placeholder="Enter email" />
+// 						<Form.Text className="text-muted">
+// 							We'll never share your email with anyone else.
+// 						</Form.Text>
+// 					</Form.Group>
+
+// 					<Form.Group className="mb-3" controlId="formBasicPassword">
+// 						<Form.Label>Password</Form.Label>
+// 						<Form.Control type="password" placeholder="Password" />
+// 					</Form.Group>
+// 					<Form.Group className="mb-3" controlId="formBasicCheckbox">
+// 						<Form.Check type="checkbox" label="Check me out" />
+// 					</Form.Group>
+// 					<Button variant="primary" type="submit">
+// 						Submit
+// 					</Button>
+// 				</Form>
+// 			</Col>
+// 		</Row>
+// 	</Container>
+// );
+
+ReactDOM.render(<GifExpertApp />, document.getElementById('root'));
